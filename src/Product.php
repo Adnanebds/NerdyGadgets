4d79b8e4f0d7548f7abe1e5160a22a30ccf9a3c3
deliviorr <s1195589@student.windesheim.nl>
2023-11-20 14:13:16 +0100

    login en regristratie

--- src/Product.php
+++ src/Product.php
@@ -4,66 +4,54 @@ $username = "root";
 $password = "";
 $db_name = "nerdy_gadgets_start";
 
-$conn = new mysqli($servername,$username,$password,$db_name);
+$conn = new mysqli($servername, $username, $password, $db_name);
 
 $sql = "SELECT * FROM product;";
-$sql_oplopend = "SELECT * FROM product ORDER BY price;";
-$sql_afloped = "SELECT * FROM product ORDER BY price DESC;";
-
 $all_product = $conn->query($sql);
-
 ?>
-
 
 <head>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="../styles/product.css">
-    </head>
+</head>
+
 <body>
-        <?php
-        include 'navbar.html';
-        ?>
-        <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 class="sr-only">Products</h2>
-        <br>
-        <div class="filter">
+<?php include 'navbar.html'; ?>
+<div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+    <h2 class="sr-only">Products</h2>
+    <br>
+    <div class="filter">
+        <?php include 'Filter.php'; ?>
+    </div>
+
+    <div class='test'>
+        <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
             <?php
-            include 'Filter.php';
+            while ($row = mysqli_fetch_assoc($all_product)) {
+                $productId = $row['id'];
+                $image = $row['image'];
+                $desc = $row['description'];
+                $prijs = $row['price'];
+                $name = $row['name'];
+                $category = $row['category'];
+                ?>
+                <a href="product_page.php?id=<?php echo $productId; ?>&image=<?php echo urlencode($image); ?>&desc=<?php echo urlencode($desc); ?>&prijs=<?php echo urlencode($prijs); ?>&name=<?php echo urlencode($name); ?>&category=<?php echo urlencode($category); ?>" class="group flex flex-col">
+                    <div class="w-full h-72 overflow-hidden rounded-lg bg-gray-200 xl:h-48 xl:w-64">
+                        <?php echo "<img src='../pngs/product_images/$image.jpg' alt='productimage' class='h-full w-full object-cover object-center group-hover:opacity-75'>"; ?>
+                    </div>
+                    <div class="p-4">
+                        <h3 class="text-xl font-bold mt-2 text-gray-800" name="product_name"><?php echo $row['name']; ?></h3>
+                        <p class="text-lg text-black" name="product_price">€<?php echo $row['price']; ?></p>
+                        <button class="bg-blue-500 text-white px-4 py-2 rounded-full mt-2 hover:bg-blue-700">Voeg toe aan winkelwagen</button>
+                    </div>
+                </a>
+                <?php
+            }
             ?>
         </div>
+    </div>
 
-            <?php
-                while($row = mysqli_fetch_assoc($all_product)){
-            ?>
-            <div class='test'>
-                <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-                    <?php
-                    foreach ($all_product as $row) {
-                        $productId = $row['id'];
-                        $image = $row['image'];
-                        ?>
-                        <a href="product_page.php?id=<?php echo $productId; ?>" class="group flex flex-col">
-                            <div class="w-full h-72 overflow-hidden rounded-lg bg-gray-200 xl:h-48 xl:w-64">
-                                <?php echo "<img src='../pngs/product_images/$image.jpg' alt='productimage' class='h-full w-full object-cover object-center group-hover:opacity-75'>"; ?>
-                            </div>
-                            <div class="p-4">
-                                <h3 class="text-xl font-bold mt-2 text-gray-800" name="product_name"><?php echo $row['name']; ?></h3>
-                                <p class="text-lg text-black" name="product_price">€<?php echo $row['price']; ?></p>
-                                <button class="bg-blue-500 text-white px-4 py-2 rounded-full mt-2 hover:bg-blue-700">Voeg toe aan winkelwagen</button>
-                            </div>
-                        </a>
-                        <?php
-                    }
-                    }
-                    ?>
-                </div>
-            </div>
-
-
-
-
-
-            <br>
+    <br>
     <br>
     <br>
     <br>
@@ -71,7 +59,6 @@ $all_product = $conn->query($sql);
     <br>
     <br><br>
     <br>
-    <?php
-        include 'footer.html';
-    ?>
+    <?php include 'footer.html'; ?>
+</div>
 </body>
---
+++ src/process-signup.php
@@ -0,0 +1,58 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name = "nerdy_gadgets_start";
+
+$conn = new mysqli($servername, $username, $password, $db_name);
+
+print_r($_POST);
+
+if (empty($_POST['name'])){
+    die('Naam is verplicht');
+}
+
+if (filter_var( ! $_POST['email'], FILTER_VALIDATE_EMAIL)){
+    die('Geldige email is verplicht');
+}
+
+if (strlen($_POST['password']) < 8){
+    die("Wachtwoord moet langer zijn dan 8 tekens");
+}
+
+if( ! preg_match('/[a-z]/i', $_POST['password'])){
+    die("Wachtwoord moet 1 of meer letter bevatten");
+}
+
+if( ! preg_match('/[0-9]/i', $_POST['password'])){
+    die("Wachtwoord moet 1 of meer cijfer bevatten");
+}
+
+if($_POST['password'] !== $_POST['password_confirmation']){
+    die("Wachtwoorden moeten hetzelfde zijn");
+}
+
+$password_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO user (first_name, email, password)
+        VALUES(?,?,?)";
+
+$stmt = $conn->stmt_init();
+
+if( ! $stmt->prepare($sql)){
+    die("SQL error:" . $conn->error);
+}
+
+$stmt->bind_param('sss',
+    $_POST['name'],
+    $_POST['email'],
+    $password_hash);
+
+if($stmt->execute()) {
+
+    header("Location: signup-succes.html");
+    exit;
+
+} else {
+    die($conn->error . ' ' . $conn->error);
+}
