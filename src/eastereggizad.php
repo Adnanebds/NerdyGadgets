fe97a550934eebff2ad76f005ec1f7b852243e67
iadamm <izadadam@hotmail.com>
2023-12-19 14:40:08 +0100

    izad easteregg

--- src/eastereggizad.php
+++ src/eastereggizad.php
@@ -1,39 +1,7 @@
-<!doctype html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../styles/output.css">
-</head>
-
-  <div class="navbar-container">
-    <?php include 'navbar.php'; ?>
-    <?php include 'hero.html'; ?>
-    <br></br>
-    <br></br>
-    <?php include 'cards.php'; ?>
-    <br></br>
-    <br></br>
-    <?php include 'testomonials.html'; ?>
-    <?php include 'footer.html'; ?>
-  </div>
-</body>
-
-
-</html>
 <!DOCTYPE html>
 <html>
 <head>
     <title>YouTube Easter Egg</title>
-    <style>
-        #videoFrame {
-            display: none;
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-        }
-    </style>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
